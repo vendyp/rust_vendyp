3f7bae825a646c95f862de1e119fb7290e51f008
Dwi Vendy Pratama <dwivendypratama@gmail.com>
2023-08-29 11:03:27 +0700

    add new api reading from query string

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,20 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+#![feature(error_generic_member_access)]
+
+use actix_web::{get, post, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use qstring::QString;
 
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
+}
+
+#[get("/say")]
+async fn say_hello(req: HttpRequest) -> String {
+    let query_str = req.query_string();
+    let qs = QString::from(query_str);
+    let name = qs.get("name").unwrap();
+
+    format!("Hello, {}!", name)
 }
 
 #[post("/echo")]
@@ -20,6 +32,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .service(hello)
             .service(echo)
+            .service(say_hello)
             .route("/hey", web::get().to(manual_hello))
     })
         .bind(("127.0.0.1", 8080))?
